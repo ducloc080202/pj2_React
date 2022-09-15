17e12ce1a63637d34adf373fb676b57f74657547
ducloc080202 <ducloc080202@gmail.com>
2022-09-15 15:05:40 +0700

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,7 @@ import './App.css'
 import LoginPage from './components/LoginPage';
 function App() {
   return (
-   <>
     <LoginPage/>
-
-    <ul>
-      {/* 
-      <Link to='/status'>status</Link>
-      <Link to='/setting'>setting</Link>
-      <Link to='/report'>report</Link> */}
-    </ul>
-   </>
   )
 }
 
